e1b33bf3189132a05dbf591e1996f68982c4ca20
Joxit <joxit972@gmail.com>
2021-03-13 00:41:26 +0100

    chore: add `npm start` server and export all components in index.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,30 +5,50 @@ import { terser } from 'rollup-plugin-terser';
 import { emptyDirectories } from 'rollup-plugin-app-utils';
 import { babel } from '@rollup/plugin-babel';
 import scss from 'rollup-plugin-scss';
+import serve from 'rollup-plugin-serve';
+import fs from 'fs';
+
+const useServe = process.env.ROLLUP_SERVE === 'true';
+const output = useServe ? '.serve' : 'build';
 
 const plugins = [
   riot(),
   nodeResolve(),
-  scss({ output: './build/styles/riot-mui.min.css', outputStyle: 'compressed' }),
+  scss({ output: `./${output}/styles/riot-mui.min.css`, outputStyle: 'compressed' }),
   commonjs(),
   babel({ babelHelpers: 'bundled', presets: ['@babel/env'] }),
 ];
 
+if (useServe) {
+  plugins.push(serve({ host: 'localhost', port: 8000, contentBase: [output, './'] }));
+  plugins.push({
+    name: 'html',
+    generateBundle: () => {
+      console.log(`Move examples/vanilla/* to ${output}/*`)
+      const html = fs
+        .readFileSync('examples/vanilla/index.html', 'utf-8')
+        .replace('../../build/js/riot-mui.js', './js/riot-mui.js');
+      fs.writeFileSync(`${output}/index.html`, html);
+      fs.writeFileSync(`${output}/demo.riot`, fs.readFileSync('examples/vanilla/demo.riot', 'utf-8'));
+    },
+  });
+}
+
 export default [
   {
-    input: 'src/index.js',
+    input: 'src/bundle.js',
     output: {
-      file: 'build/js/riot-mui-min.js',
+      file: `${output}/js/riot-mui-min.js`,
       format: 'iife',
       globals: { 'riot': 'riot' },
     },
     external: ['riot'],
-    plugins: [emptyDirectories('build')].concat(plugins, terser()),
+    plugins: [emptyDirectories(output)].concat(plugins, terser()),
   },
   {
-    input: 'src/index.js',
+    input: 'src/bundle.js',
     output: {
-      file: 'build/js/riot-mui.js',
+      file: `${output}/js/riot-mui.js`,
       format: 'iife',
       globals: { 'riot': 'riot' },
     },
---
+++ src/bundle.js
@@ -0,0 +1,36 @@
+import {
+  register
+} from 'riot'
+
+import * as mui from './';
+
+import './material-elements/material-button/material-button.scss';
+import './material-elements/material-card/material-card.scss';
+import './material-elements/material-checkbox/material-checkbox.scss';
+import './material-elements/material-combo/material-combo.scss';
+import './material-elements/material-dropdown/material-dropdown.scss';
+import './material-elements/material-dropdown-list/material-dropdown-list.scss';
+import './material-elements/material-footer/material-footer.scss';
+import './material-elements/material-input/material-input.scss';
+import './material-elements/material-navbar/material-navbar.scss';
+import './material-elements/material-pane/material-pane.scss';
+import './material-elements/material-popup/material-popup.scss';
+import './material-elements/material-snackbar/material-snackbar.scss';
+import './material-elements/material-spinner/material-spinner.scss';
+import './material-elements/material-tabs/material-tabs.scss';
+import './material-elements/material-textarea/material-textarea.scss';
+import './material-elements/material-waves/material-waves.scss';
+
+register('material-button', mui.MaterialButton);
+register('material-card', mui.MaterialCard);
+register('material-checkbox', mui.MaterialCheckbox);
+register('material-dropdown', mui.MaterialDropdown);
+register('material-dropdown-list', mui.MaterialDropdownList);
+register('material-footer', mui.MaterialFooter);
+register('material-input', mui.MaterialInput);
+register('material-navbar', mui.MaterialNavbar);
+register('material-pane', mui.MaterialPane);
+register('material-popup', mui.MaterialPopup);
+register('material-spinner', mui.MaterialSpinner);
+register('material-tabs', mui.MaterialTabs);
+register('material-waves', mui.MaterialWaves);
