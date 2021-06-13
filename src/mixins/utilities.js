0cffd6e30e7362dbc7d23a9e8e87b2920292ad69
Joxit <joxit972@gmail.com>
2021-06-13 13:13:13 +0200

    fix: add missing utilities

--- src/mixins/utilities.js
+++ src/mixins/utilities.js
@@ -1,16 +1,36 @@
-
 const attributeToBool = (attribute) => {
   if (attribute === '' || attribute === 'true') {
     return true;
   }
   return false;
-}
+};
 
-const tryCall = function(fn) {
+const tryCall = function (fn) {
   if (typeof fn === 'function') {
-    const deconstruction = new Array(...arguments).slice(1)
-    fn(...deconstruction)
+    const deconstruction = new Array(...arguments).slice(1);
+    fn(...deconstruction);
   }
 };
 
-export { attributeToBool, tryCall };
+const HTML = {};
+
+HTML.array = (element) => {
+  if (!element) {
+    return [];
+  }
+  let array = [];
+  for (let i = 0; i < element.length; i++) {
+    array.push(element.item(i));
+  }
+  return array;
+};
+
+HTML.children = (element) => HTML.array(element && element.children);
+
+const attributesObj = (supported, props) =>
+  supported.reduce((attributes, attr) => {
+    attributes[attr] = attributeToBool(props[attr]);
+    return attributes;
+  }, {});
+
+export { attributeToBool, tryCall, HTML, attributesObj };
