7bf3e7832da4af2e3acfac4760459f1d397c7d9b
Joxit <joxit972@gmail.com>
2021-07-16 19:19:19 +0200

    docs: update home and add new links

--- docs/src/index.js
+++ docs/src/index.js
@@ -2,7 +2,22 @@ import { component } from 'riot';
 
 import Documentation from './riot-mui-documentation.riot';
 
-import 'riot-mui/build/styles/riot-mui.min.css'
+import '../../src/material-elements/material-button/material-button.scss';
+import '../../src/material-elements/material-card/material-card.scss';
+import '../../src/material-elements/material-checkbox/material-checkbox.scss';
+import '../../src/material-elements/material-combo/material-combo.scss';
+import '../../src/material-elements/material-dropdown/material-dropdown.scss';
+import '../../src/material-elements/material-dropdown-list/material-dropdown-list.scss';
+import '../../src/material-elements/material-footer/material-footer.scss';
+import '../../src/material-elements/material-input/material-input.scss';
+import '../../src/material-elements/material-navbar/material-navbar.scss';
+import '../../src/material-elements/material-pane/material-pane.scss';
+import '../../src/material-elements/material-popup/material-popup.scss';
+import '../../src/material-elements/material-snackbar/material-snackbar.scss';
+import '../../src/material-elements/material-spinner/material-spinner.scss';
+import '../../src/material-elements/material-tabs/material-tabs.scss';
+import '../../src/material-elements/material-textarea/material-textarea.scss';
+import '../../src/material-elements/material-waves/material-waves.scss';
 
 const createApp = component(Documentation);
 const tags = document.getElementsByTagName('riot-mui-documentation');
